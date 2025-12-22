f59f9f581eb66456dc5e4682be30896b75077035
unknown <alelybitun.school@gmail.com>
2025-12-22 11:04:01 +0800

    feat: Add get_time_slots.php

---
+++ get_time_slots.php
@@ -0,0 +1,35 @@
+<?php
+// get_time_slots.php
+
+require_once 'config.php';
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Content-Type');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    echo json_encode(['success' => false, 'message' => 'Method not allowed']);
+    exit();
+}
+
+$sql = "SELECT time_ID AS id, display_name FROM time";
+
+$result = $conn->query($sql);
+if (!$result) {
+    echo json_encode(['success' => false, 'message' => 'Database error: ' . $conn->error]);
+    exit();
+}
+
+$timeSlots = [];
+while ($row = $result->fetch_assoc()) {
+    $timeSlots[] = $row;
+}
+
+echo json_encode([
+    'success' => true,
+    'time_slots' => $timeSlots
+]);
+
+$conn->close();
+?>
